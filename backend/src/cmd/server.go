6f0d5251de5afea695c2950da756264add8723e3
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2026-01-01 06:18:31 +0700

    feat: update dependencies and add CORS middleware configuration

--- backend/src/cmd/server.go
+++ backend/src/cmd/server.go
@@ -1,15 +1,27 @@
 package cmd
 
 import (
+	"time"
 	"yaoyao-functions/src/config"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
+
 	"gorm.io/gorm"
 )
 
 func Start(db *gorm.DB, redisClient *redis.Client) *gin.Engine {
 	router := gin.Default()
+
+  router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173", "https://yaoyaodinner.party"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+  }))
+
 	config.RegisterModules(router, db, redisClient)
 	return router
 }
