dc7e40dcd0cefc1746198a8f2262b0009661e128
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2026-01-03 07:45:47 +0700

    feat: update CORS configuration to allow requests from www.yaoyaodinner.party

--- backend/src/config/cors.go
+++ backend/src/config/cors.go
@@ -8,7 +8,7 @@ import (
 )
 
 func RegisterCors(router *gin.Engine) {  router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "https://yaoyaodinner.party"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://yaoyaodinner.party", "https://www.yaoyaodinner.party"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
